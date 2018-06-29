3b147c53cd15ffa9d004c41c9c4f5b2663e21cd9
sinjie11 <sinjiezzang@hanmail.net>
2018-06-29 12:44:46 +0900

    Message 기능 플러그인 추가.(pom.xml)

--- PumpkinPlace/src/main/java/com/error404/pumpkinplace/domain/Message.java
+++ PumpkinPlace/src/main/java/com/error404/pumpkinplace/domain/Message.java
@@ -4,18 +4,19 @@ import java.util.Date;
 
 public class Message {
 	
-	private long msg_no; // 쪽지 번호
+	private int msg_no; // 쪽지 번호
 	private String mem_id; // 회원 아이디
 	private String msg_title; // 쪽지 제목
 	private String msg_content; // 쪽지 내용
 	private String msg_send; // 쪽지 보낸 사람
 	private String msg_recieve; // 쪽지 받는 사람
 	private Date msg_regdate; // 쪽지 작성일자
+	private int msg_section; // 쪽지 구분(1: 보낸편지함, 2: 받은편지함)
 	
 	public Message() {}
 
-	public Message(long msg_no, String mem_id, String msg_title, String msg_content, String msg_send,
-			String msg_recieve, Date msg_regdate) {
+	public Message(int msg_no, String mem_id, String msg_title, String msg_content, String msg_send, String msg_recieve,
+			Date msg_regdate, int msg_section) {
 		super();
 		this.msg_no = msg_no;
 		this.mem_id = mem_id;
@@ -24,13 +25,14 @@ public class Message {
 		this.msg_send = msg_send;
 		this.msg_recieve = msg_recieve;
 		this.msg_regdate = msg_regdate;
+		this.msg_section = msg_section;
 	}
 
-	public long getMsg_no() {
+	public int getMsg_no() {
 		return msg_no;
 	}
 
-	public void setMsg_no(long msg_no) {
+	public void setMsg_no(int msg_no) {
 		this.msg_no = msg_no;
 	}
 
@@ -82,5 +84,13 @@ public class Message {
 		this.msg_regdate = msg_regdate;
 	}
 
+	public int getMsg_section() {
+		return msg_section;
+	}
 
+	public void setMsg_section(int msg_section) {
+		this.msg_section = msg_section;
+	}
+
+	
 } // end class Message
