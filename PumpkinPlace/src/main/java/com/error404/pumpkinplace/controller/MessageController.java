67fa5ca68e5af859b8dd85dfe80e5c95dad899b0
sinjie11 <sinjiezzang@hanmail.net>
2018-07-02 17:32:20 +0900

    MessageController 생성 및 jsp 연동.

---
+++ PumpkinPlace/src/main/java/com/error404/pumpkinplace/controller/MessageController.java
@@ -0,0 +1,29 @@
+package com.error404.pumpkinplace.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.error404.pumpkinplace.service.MemberService;
+
+@Controller
+@RequestMapping(value = "/message")
+public class MessageController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	@Autowired
+	private MemberService memberService;
+	
+	@RequestMapping(value = "/message", method = RequestMethod.GET)
+	public void insertMessage() {
+		logger.info("insertMessage() GET 호출");
+		
+	} // end insertMessage()
+	
+	
+
+} // end class MessageController
