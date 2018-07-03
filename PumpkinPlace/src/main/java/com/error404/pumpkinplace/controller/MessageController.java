781a97f7f7d4d667346f8d6e34acef577b4448c2
sinjie11 <sinjiezzang@hanmail.net>
2018-07-03 18:45:09 +0900

    MessageController 수정.

--- PumpkinPlace/src/main/java/com/error404/pumpkinplace/controller/MessageController.java
+++ PumpkinPlace/src/main/java/com/error404/pumpkinplace/controller/MessageController.java
@@ -1,13 +1,18 @@
 package com.error404.pumpkinplace.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.error404.pumpkinplace.domain.Message;
 import com.error404.pumpkinplace.service.MemberService;
+import com.error404.pumpkinplace.service.MessageService;
 
 @Controller
 @RequestMapping(value = "/message")
@@ -16,10 +21,10 @@ public class MessageController {
 	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
 	
 	@Autowired
-	private MemberService memberService;
+	private MessageService messageService;
 	
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public void messageList() {
+	public void messageList(Model model) {
 		logger.info("messageList() GET 호출");
 		
 	} // end messageList()
@@ -30,17 +35,30 @@ public class MessageController {
 		
 	} // end insertMessage()
 	
-	@RequestMapping(value = "/send", method = RequestMethod.GET)
-	public void selectById() {
-		logger.info("selectById() GET 호출");
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	public String insertMessage(Message message) {
+		logger.info("insertMessage({}) POST 호출", message);
+		messageService.create(message);
 		
-	} // end selectById()
+		return "redirect:/message/list";
+		
+	} // end insertMessage()
+	
+	@RequestMapping(value = "/send", method = RequestMethod.GET)
+	public void send(String mem_id, Model model) {
+		Message message = new Message(0, "${loginId}", null, null, null, null, false);
+		
+		List<Message> list = messageService.send(message.getMem_id());
+		model.addAttribute("sendList", list);
+		
+		
+	} // end send(mem_id, model)
 	
 	@RequestMapping(value = "/recieve", method = RequestMethod.GET)
-	public void selectById2() {
-		logger.info("selectById()2 GET 호출");
+	public void recieve() {
+
 		
-	} // end selectById()
+	} // end recieve()
 	
 	
 
--- PumpkinPlace/src/main/java/com/error404/pumpkinplace/persistence/MessageDao.java
+++ PumpkinPlace/src/main/java/com/error404/pumpkinplace/persistence/MessageDao.java
@@ -1,9 +1,15 @@
 package com.error404.pumpkinplace.persistence;
 
+import java.util.List;
+
 import com.error404.pumpkinplace.domain.Message;
 
 public interface MessageDao {
 	
+	List<Message> send(String mem_id);
+	
+	List<Message> recieve(String mem_id2);
+		
 	int insert(Message message);
 	
 	Message select(String mem_id);
