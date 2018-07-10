12062a31c76f1e844e9fb3a94d24c80ae78c9fd1
suhyeoney <sooheyong102@gmail.com>
2018-07-10 15:40:11 +0900

    Re push

--- PumpkinPlace/src/main/java/com/error404/pumpkinplace/controller/GameController.java
+++ PumpkinPlace/src/main/java/com/error404/pumpkinplace/controller/GameController.java
@@ -2,9 +2,15 @@ package com.error404.pumpkinplace.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.error404.pumpkinplace.domain.Game;
+import com.error404.pumpkinplace.service.GameService;
 
 @Controller
 @RequestMapping(value = "/game")
@@ -12,11 +18,27 @@ public class GameController {
 
 	private static final Logger logger = LoggerFactory.getLogger(GameController.class);
 	
-
-	@RequestMapping(value = "/gamepage")
-	public void game() {
-		logger.info("game 페이지 호출");
-		
-	}
-
+//	@Autowired
+//	private GameService gameService;
+//
+//	@RequestMapping(value = "/register", method=RequestMethod.GET)
+//	public void register() {	
+//	}
+//	@RequestMapping(value = "/register",  method=RequestMethod.POST)
+//	public String register(Game game) {
+//		logger.info("game 페이지 호출");
+//		gameService.insert(game);
+//		
+//		return "redirect:/game/";
+//	}
+//	
+//	@RequestMapping(value = "/update", method=RequestMethod.GET)
+//	public void update(int mem_no, Game game, RedirectAttributes attr) {
+//		int result = gameService.update(game);
+//		if(result == 1)
+//			attr.addFlashAttribute("updateResult", "success");
+//		
+//		
+//	}
 }
+
